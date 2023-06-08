6f3c938128dcb0245f90cd23d156562c31c39b13
Alexey Stetsenko <fedorov091@gmail.com>
2023-06-08 21:27:15 +0300

    новые изменения

---
+++ Homework10/task_with_star.py
@@ -0,0 +1,14 @@
+# Есть маркер @pytest.mark.id_check(1, 2, 3), нужно вывести на печать, то что в него передано
+#
+# >>> 1, 2, 3
+
+import pytest
+
+
+@pytest.mark.id_check(1, 2, 3)
+def test(request):
+    # Здесь пишем код
+    marker = request.node.get_closest_marker("id_check")
+    args = marker.args
+    print(*args, sep=", ")
+    pass
